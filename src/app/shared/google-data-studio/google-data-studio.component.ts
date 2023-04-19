2898b552076396810f04550e51eac7831b588909
rsakhidas <89382725+rsakhidas@users.noreply.github.com>
2023-04-19 17:40:55 +0530

    Changes in mapbox widget

--- src/app/shared/google-data-studio/google-data-studio.component.ts
+++ src/app/shared/google-data-studio/google-data-studio.component.ts
@@ -117,6 +117,7 @@ export class GoogleDataStudioComponent implements OnInit {
   }
 
   setMarkers() {
+    let list: any = [];
     for (let item of this.siteList) {
       this.getColorSiteWise(item);
       let obj = {
@@ -128,13 +129,23 @@ export class GoogleDataStudioComponent implements OnInit {
         'properties': {
           'title': item.smSitecode,
           'message': item.smSitename,
+          'description': `<strong>${item.smSitecode}</strong><p>${item.smSitename}</p>`,
           'bgColor': item.bgColor,
           'customerId': item.smCustomerId,
           'smSitetypeid': item.smSitetypeid
         }
       };
+      list.push(obj);
       this.createMarker(obj);
     }
+    this.map.addSource('places', {
+      'type': 'geojson',
+      'data': {
+        'type': 'FeatureCollection',
+        'features': list
+      }
+    })
+    this.addPopupOnMarker();
   }
 
   createMarker(marker: any) {
@@ -160,6 +171,53 @@ export class GoogleDataStudioComponent implements OnInit {
     for (let item of this.markers) {
       item.remove();
     }
+  }
+
+  addPopupOnMarker() {
+
+    // Add a layer showing the places.
+    this.map.addLayer({
+      'id': 'places',
+      'type': 'circle',
+      'source': 'places',
+      'paint': {
+        'circle-color': '#4264fb',
+        'circle-radius': 6,
+        'circle-stroke-width': 2,
+        'circle-stroke-color': '#ffffff'
+      }
+    });
+
+    // Create a popup, but don't add it to the map yet.
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+
+    this.map.on('mouseenter', 'places', (e) => {
+      // Change the cursor style as a UI indicator.
+      this.map.getCanvas().style.cursor = 'pointer';
+
+      // Copy coordinates array.
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.description;
+
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup.setLngLat(coordinates).setHTML(description).addTo(this.map);
+    });
+
+    this.map.on('mouseleave', 'places', () => {
+      this.map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
   }
 
   search(evt?: any) {
