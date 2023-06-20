d8f01233d552a238ed4d72c3e53d8887aabbda7c
rsakhidas <89382725+rsakhidas@users.noreply.github.com>
2023-06-20 15:26:34 +0530

    Solved feedback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // api: 'http://54.254.44.119:8080/digitrinityApi/api'
-  api: 'http://54.254.44.119:8080/digitrinity-rest-services/api',
-  prodAPI: 'http://54.254.44.119:8080/digitrinity-rest-services_prod',
+  // api: 'http://54.254.44.119:8080/digitrinity-rest-services/api',
+  api: 'http://54.254.44.119:8080/digitrinity-rest-services_prod',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiZGlnaXRyaW5pdHkiLCJhIjoiY2thMjdzaDY1MDNmZzNmbXN1Yjl4aXp5bSJ9.2rBqqFjtw-u4qtDSFkOR0w'
   }
