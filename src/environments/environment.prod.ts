89747bb7b28ad92a530f098e3d9131fa0aa3b1e0
Noor <noor.saleem409@gmail.com>
2026-05-23 23:48:54 +0530

    updated docker file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,7 @@
 export const environment = {
   production: true,
-  // api: 'http://54.254.44.119:8080/digitrinityApi/api'
-  // api: 'http://54.254.44.119:8080/digitrinity-rest-services/api',
-  // 
-       api:'https://rms.yomamicropowerservice.com:8443/digitrinity-rest-services_prod',
-
-
+  api: 'http://18.232.167.217:8080/digitrinity-rest-services_prod',
   mapbox: {
     accessToken: 'pk.eyJ1IjoiZGlnaXRyaW5pdHkiLCJhIjoiY2thMjdzaDY1MDNmZzNmbXN1Yjl4aXp5bSJ9.2rBqqFjtw-u4qtDSFkOR0w'
   }
-
-  // api: 'http://54.254.44.119:8081/digitrinity/api'
-  // http://54.254.44.119:8080/digitrinity/prf_dashboard/site_summary
 };
